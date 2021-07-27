cafdcab9aca01087781ed43d19e0cefb1a036f2e
AntonioHoracio77 <80331629+AntonioHoracio77@users.noreply.github.com>
2021-07-26 21:21:20 -0300

    Create 3126.c

---
+++ 3126.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ int n,x,cont=0;
+ scanf("%d",&n);
+ while(n--){
+     scanf("%d",&x);
+     if(x==1){
+         cont++;
+     }
+ }
+ printf("%d\n",cont);
+    return 0;
+}
